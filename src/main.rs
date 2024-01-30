aa5f5a5115d764518bb5a993b12e8219b28d042d
JethroNatividad <natividad.jet@gmail.com>
2024-01-31 05:48:01 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 // Write a program that has a list of names, prompt a name to remove it from the list.
+// Inputs: employee_name
+// Process: search in list, remove
+// Outputs: the updated employee list
 
 fn main() {
     println!("Hello, world!");
