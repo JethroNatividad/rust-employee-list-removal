affea316af7d4d77109b1130f38bcbddccb7e6ae
JethroNatividad <natividad.jet@gmail.com>
2024-02-01 07:13:46 +0800

    fix tests

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@
 // Process: search in list, remove
 // Outputs: the updated employee list
 
-fn remove_employee(employees: Vec<String>, remove_name: &str) -> Result<Vec<String>, &'static str> {
+fn remove_employee(
+    employees: Vec<String>,
+    remove_name: String,
+) -> Result<Vec<String>, &'static str> {
     if employees.contains(&remove_name) {
         Ok(employees
             .iter()
@@ -30,13 +33,18 @@ mod tests {
         ];
 
         assert_eq!(
-            remove_employee(&employees, "John Smith"),
-            vec![
-                "Jackie Jackson",
-                "Chris Jones",
-                "Amanda Cullen",
-                "Jeremy Goodwin"
-            ]
+            remove_employee(employees.clone(), "John Smith".to_string()),
+            Ok(vec![
+                "Jackie Jackson".to_string(),
+                "Chris Jones".to_string(),
+                "Amanda Cullen".to_string(),
+                "Jeremy Goodwin".to_string()
+            ])
+        );
+
+        assert_eq!(
+            remove_employee(employees, "Nonexistent Employee".to_string()),
+            Err("String not found in vector")
         );
     }
 }
