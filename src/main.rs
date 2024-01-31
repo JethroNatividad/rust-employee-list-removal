417dcd3aa51e9c81e2220d306f57cc7fc2d7b7ee
JethroNatividad <natividad.jet@gmail.com>
2024-01-31 20:28:15 +0800

    update tests and remove employee params

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 // Process: search in list, remove
 // Outputs: the updated employee list
 
-fn remove_employee(employees: &mut Vec<&str>, remove_name: &str) -> Result<(), &'static str> {}
+fn remove_employee(employees: Vec<&str>, remove_name: &str) -> Result<Vec<&str>, &'static str> {}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -19,7 +19,7 @@ mod tests {
         ];
 
         assert_eq!(
-            remove_employee(employees, "John Smith"),
+            remove_employee(employees.clone(), "John Smith"),
             vec![
                 "Jackie Jackson",
                 "Chris Jones",
